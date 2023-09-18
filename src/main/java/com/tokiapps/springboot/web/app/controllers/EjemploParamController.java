2b2b3d35638b61ac47be419096ac1c0f528f85f9
Alberto Gonzalez <43555310+T0kiii@users.noreply.github.com>
2023-09-18 11:37:20 +0000

    Emplear HttpServletRequest

--- src/main/java/com/tokiapps/springboot/web/app/controllers/EjemploParamController.java
+++ src/main/java/com/tokiapps/springboot/web/app/controllers/EjemploParamController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/params")
 public class EjemploParamController {
@@ -23,6 +25,21 @@ public class EjemploParamController {
 	
 	@GetMapping("/mix-params")
 	public String param(@RequestParam String saludo, Integer numero, Model model) {
+		model.addAttribute("resultado", "El saludo enviado es " + saludo + " y el número: " + numero);
+		return "/params/ver";
+	}
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+
+		} catch (NumberFormatException e) {
+			numero = 0;
+		}
+
 		model.addAttribute("resultado", "El saludo enviado es " + saludo + " y el número: " + numero);
 		return "/params/ver";
 	}
