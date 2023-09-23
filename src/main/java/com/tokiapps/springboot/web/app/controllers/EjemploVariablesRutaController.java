9234a541f3e999aac83c75c13b48636f7596c805
Alberto Gonzalez <43555310+T0kiii@users.noreply.github.com>
2023-09-23 18:45:14 +0000

    @PathVariable con varias variables

--- src/main/java/com/tokiapps/springboot/web/app/controllers/EjemploVariablesRutaController.java
+++ src/main/java/com/tokiapps/springboot/web/app/controllers/EjemploVariablesRutaController.java
@@ -10,10 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class EjemploVariablesRutaController {
     
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("titulo", "Enviar parámetros de la ruta (@PathVariable)");
+        return "variables/index";
+    }
+
     @GetMapping("/string/{texto}")
     public String variables(@PathVariable(name = "texto") String texto, Model model){
         model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
         model.addAttribute("resultado", "El texto recibido es: " + texto);
+        return "variables/ver";
+    }
+    
+    @GetMapping("/string/{texto}/{numero}")
+    public String variables(@PathVariable String texto, @PathVariable Integer numero, Model model){
+        model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
+        model.addAttribute("resultado", "El texto recibido es: " + texto
+                            + " y el número es: " + numero);
         return "variables/ver";
     }
 }
